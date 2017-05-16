54984f94b6ba2dc7a52f8ea8f5e11a50b33fc854
td2014 <anthonyd2004@gmail.com>
2017-05-16 12:41:00 -0700

    Completed part 1

--- my_answers.py
+++ my_answers.py
@@ -12,7 +12,23 @@ def window_transform_series(series,window_size):
     # containers for input/output pairs
     X = []
     y = []
-
+    
+    # set a sample index counter for the return array X
+    sample_index=0
+    # Loop over the input series until data is exhausted, with sliding window
+    while sample_index+window_size < len(series):
+        # create a sample vector at each sample which covers the input window
+        sample=[]
+        sample[0:window_size] = \
+           series[sample_index:sample_index+window_size]
+        
+        # update windowed input
+        X.append(sample)
+        # obtain the next value beyond the window as the output for the current sample    
+        y.append(series[sample_index+window_size])
+        # advance the window by one time step
+        sample_index=sample_index+1
+        
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,7 +39,25 @@ def window_transform_series(series,window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(step_size, window_size):
-    pass
+#
+# Using specifications from Jupyter notebook instructions.
+# Don't know what to do with step_size here.  Ignoring for now.
+#
+    model = Sequential()
+# Add recurrant module with 5 hidden units
+    model.add(LSTM(5, input_shape=(1,window_size)))
+# Add fully connected module for output - 1 output unit
+    model.add(Dense(1))
+# Verify architecture
+    model.summary()
+
+# build model using keras documentation recommended optimizer initialization
+    optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+# compile the model
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+# return model and exit
+    return model
 
 
 ### TODO: list all unique characters in the text and remove any non-english ones
